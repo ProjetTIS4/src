ef80a7671219b75efbab2b8b34cecf42db36a393
lel923 <serena.sailey@hotmail.fr>
2022-02-09 15:23:59 +0100

    Test

--- UI/Connexion.java
+++ UI/Connexion.java
@@ -13,6 +13,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
 import java.awt.Insets;
+import java.awt.image.BufferedImage;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -47,25 +49,25 @@ public class Connexion implements Runnable {
         connexion.setBounds(450, 190, 700, 460);
         connexion.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
- 
+        // Création de tous les éléments apparaissant dans la fenêtre 
         //Image
         ImageIcon icone = new ImageIcon("src/Annexes/Hubspital_logo.png");
         Image img = icone.getImage(); // Convertissemnt pour pouvoir redimensionner
         Image newimg = img.getScaledInstance(344, 460, java.awt.Image.SCALE_SMOOTH); // On choisit la taille de l'image
-       
+
         icone = new ImageIcon(newimg); // On reconvertit
         JLabel image = new JLabel(icone);
-        
-        //Fond image
         image.setOpaque(true);
         image.setBackground(WHITE);
-        
 
+        //fond image 
         connexion.setVisible(true); // On permet à la fenêtre de s'afficher
 
         connexion.getContentPane().add(image, BorderLayout.WEST); //On ajoute l'image dans la partie gauche de notre fenêtre
 
         //Partie Connexion
+        JLabel connexionTexte = new JLabel("Connexion ");
+        connexionTexte.setFont(new Font("Bookman Old Style", Font.PLAIN, 25));
         JLabel utilisateur = new JLabel("Nom d'utilisateur : "); //Le texte "Nom d'utilisateur :"
         utilisateur.setFont(new Font("Bookman Old Style", Font.PLAIN, 18)); //On modifie la police et la taille de l'écriture
         JLabel mdp = new JLabel("Mot de passe : "); // Le texte "Mot de passe :"
@@ -74,57 +76,61 @@ public class Connexion implements Runnable {
         erreur.setForeground(Color.RED);
 
         Valider = new JButton("Se connecter"); // Le bouton "Se connecter"
+        //   Valider.setBackground(new Color(100, 180, 180));  //Pour changer la couleur du bouton 
 
         textUtilisateur.setMaximumSize(new Dimension(500, textUtilisateur.getMinimumSize().height)); //On choisit la taille de la barre de texte utilisateur
         jPasswordField1.setMaximumSize(new Dimension(500, jPasswordField1.getMinimumSize().height)); //On choisit la taille de la barre de texte mot de passe
 
-        
-        
         JPanel jPanel = new JPanel(new GridBagLayout());
-        JPanel jPanel1 = new JPanel(); // Ce panel va contenir tous les composants créés dans la partie Connexion
-        jPanel1.setLayout(new BoxLayout(jPanel1, BoxLayout.Y_AXIS));
 
-
-        /* Ajout de ces composants en spécifiant les contraintes de type GridBagConstraints. */
+        /*3- Ajout de ces composants en spécifiant les contraintes de type GridBagConstraints. */
         GridBagConstraints gbc = new GridBagConstraints();
 
+        /*Texte "Connexion" en haut de la fenêtre*/
         gbc.gridx = 1;
         gbc.gridy = 0;
-        gbc.insets = new Insets(10,8,0,0); 
+        gbc.insets = new Insets( 0, 0, 0, 0);
+        gbc.weighty = 1;
+        gbc.anchor = GridBagConstraints.CENTER;
+        jPanel.add(connexionTexte, gbc);
+
+        /*Texte "Utilisateur" */
+        
+         gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(0, 8, 0, 0);
+        gbc.anchor = GridBagConstraints.PAGE_START;
         jPanel.add(utilisateur, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.insets = new Insets(5,0,0,0); 
-        gbc.ipadx=150;
+        /*Champ de texte Utilisateur */
+        gbc.insets = new Insets(30, 0, 0, 0);
+        gbc.ipadx = 150;
         jPanel.add(textUtilisateur, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridy = 2;
-        gbc.insets = new Insets(10,0,0,0); 
-        gbc.ipadx=0;
+        /*Texte "Mot de passe" */
+        gbc.insets = new Insets(60, 0, 0, 0);
+        gbc.ipadx = 0;
         jPanel.add(mdp, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridy = 3;
-        gbc.insets = new Insets(5,0,0,0); 
-        gbc.ipadx=150;
+        /*Champ de texte Mot de Passe */
+        gbc.insets = new Insets(90, 0, 0, 0);
+        gbc.ipadx = 150;
         jPanel.add(jPasswordField1, gbc);
-        
+
+        /*Bouton valider*/
         gbc.gridx = 1;
-        gbc.gridy = 5;
-        gbc.insets = new Insets(60,0,0,0); 
-        gbc.ipadx=0;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(160, 0, 0, 0);
+        gbc.ipadx = 0;
+       gbc.anchor = GridBagConstraints.PAGE_START;
         jPanel.add(Valider, gbc);
-        
-        
+
+
 
         connexion.add(jPanel, BorderLayout.CENTER); // On ajoute le panel final au centre de la fenêtre
 
-        
         jPanel.setBackground(LIGHT_BLUE);
-        
-       
+
     }
 
 }
